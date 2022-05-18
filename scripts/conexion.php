bec445da2174ce05888c8177218724c7166c36e4
sharon2110 <105511530+sharon2110@users.noreply.github.com>
2022-05-18 16:26:11 -0600

    Update conexion.php

--- scripts/conexion.php
+++ scripts/conexion.php
@@ -1,12 +1,10 @@
 <?php
-
-
-$db="id18908801_ascmotors2110";
-$username = "utkzf2xu6vocpfxk";
-$password="imPu7ZRGYNzUT9MuXees";
+$db="basemotors";
+$username = "uofwg7ny4iayy9vr";
+$password="Us65MNPpSEneTJS2KZsY";
 
 try{
-   $con = new PDO("mysql:host=bs1yhrxmrexwadxaayyb-mysql.services.clever-cloud.com;port=3306;dbname=bs1yhrxmrexwadxaayyb",$username,$password);
+   $con = new PDO('mysql:host=bbyhlfffxo0hldaxg1zt-mysql.services.clever-cloud.com;dbname=bbyhlfffxo0hldaxg1zt',$username,$password);
    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    
 }catch(PDOException $e){
